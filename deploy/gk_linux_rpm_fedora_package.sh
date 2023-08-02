25da37bb3dc77ca175b34c698177f322b6b5541a
Roman G <guard43ru@gmail.com>
2023-08-02 04:51:45 +0400

    test github actions

--- deploy/gk_linux_rpm_fedora_package.sh
+++ deploy/gk_linux_rpm_fedora_package.sh
@@ -9,9 +9,9 @@ mkdir -p ~/rpmbuild/SOURCES
 mkdir -p ~/rpmbuild/SPECS
 mkdir -p ~/rpmbuild/SRPMS
 
-ls -la ../plugins/
-#find ../plugins/runtime/ -type d -not -name 'linux-x64' -exec rm -rf {} \;
-#ls -la ../plugins/runtime/
+ls -la ../plugins/runtimes/
+find ../plugins/runtimes/ -type d -not -name 'linux-x64' -exec rm -rf {} \;
+ls -la ../plugins/runtimes/
 
 tar -zcf ~/rpmbuild/SOURCES/gedkeeper.tar.gz -T "$DIR/rpm/gk_files.txt"
 cp "$DIR/rpm/gedkeeper.spec" ~/rpmbuild/SPECS/gedkeeper.spec
